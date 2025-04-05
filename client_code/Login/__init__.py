8d3a83f8f139b51a6ca1cf34370ffe2a891c1f3e
alfonsogarijo@hotmail.com <alfonsogarijo@hotmail.com>
2025-04-05 20:42:48 +0000

    Created Login; Edited theme.css and settings; Deleted Form1

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import LoginTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -8,10 +8,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.background = "asset:FondoApp.png"
 
     # Any code you write here will run before the form opens.
